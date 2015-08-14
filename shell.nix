ad91f0ea844da8647430a2d835469f898bb2fb04
Nick Spinale <spinalen@carleton.edu>
2015-08-14 06:41:54 +0000

    basic types and parser

---
+++ shell.nix
@@ -0,0 +1,20 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7101" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, attoparsec, base, stdenv }:
+      mkDerivation {
+        pname = "untyped-lambda-calculus";
+        version = "0.1.0.0";
+        src = ./src;
+        buildDepends = [ attoparsec base ];
+        license = stdenv.lib.licenses.mit;
+      };
+
+  drv = pkgs.haskell.packages.${compiler}.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
--- src/Language/Lambda.hs
+++ src/Language/Lambda.hs
@@ -1,16 +1,50 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-module Data.Fin
-    ( 
+module Language.Lambda
+    (
     ) where
 
-data Zero
-data Succ n
+import Control.Applicative
+import Data.Attoparsec.ByteString.Char8
 
-data Fin n where
-    Zero :: Fin n
-    Succ :: Fin n -> Fin (Succ n)
+-- Naturals for use on the type-level
+data Nat = Z | S Nat
 
-data Expr n = Variable (Fin n)
-            | Application (Expr n) (Expr n)
-            | Abstraction (Expr (Succ n))
+-- Finite totally ordered sets, indexed by the naturals
+data Fin (n :: Nat) where
+    Zero :: Fin (S n)
+    Succ :: Fin n -> Fin (S n)
+
+value :: Fin n -> Integer
+value Zero = 0
+value (Succ n) = value n
+
+instance Show (Fin n) where
+    show = show . value
+
+-- De Bruijn indexed lambda expressions, where variables are labeled with t's
+data Term (n :: Nat) = Variable (Fin n)
+                     | Application (Term n) (Term n)
+                     | Abstraction (Term (S n))
+  deriving Show
+
+data Expr = Variable' Identifier
+          | Application' Expr Expr
+          | Abstraction' Identifier Expr
+  deriving Show
+
+newtype Identifier = Identifier { unwrap :: String }
+  deriving (Eq, Show)
+
+expr :: Parser Expr
+expr = char '(' *> expr' <* char ')'
+  where
+    expr' :: Parser Expr
+    expr' = Variable' <$> identifier
+         <|> Application' <$> expr <*> expr
+         <|> Abstraction' <$> (string "\\ " *> identifier <* string " . ") <*> expr'
+    identifier :: Parser Identifier
+    identifier = Identifier <$> many1 letter_ascii
