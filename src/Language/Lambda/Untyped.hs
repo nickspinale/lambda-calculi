a3737ae8e37fa83b695df1fdad0cbd65e6e9c0cb
Nick Spinale <spinalen@carleton.edu>
2015-08-16 02:51:39 +0000

    working on beta reduction

--- src/Language/Lambda/Untyped.hs
+++ src/Language/Lambda/Untyped.hs
@@ -9,13 +9,38 @@ module Language.Lambda.Untyped
     (
     ) where
 
-import Data.Attoparsec.ByteString.Char8
-import Data.ByteString.Builder
-import Data.Proxy
-import Control.Applicative
+import           Data.Attoparsec.ByteString.Char8
+import qualified Data.ByteString.Builder as B
+import           Data.Proxy
+import           Control.Applicative
+import           Control.Monad
+import           Data.Monoid
 
 data Nat = Z | S Nat
-  deriving Show
+  deriving (Ord, Eq, Show)
+
+-- instance Num Nat where
+--     n + Z = n
+--     n + S m = S n + m
+--     n - Z = n
+--     S n - S m = n - m
+--     Z - S _ = error "can't subtract S _ from Z"
+--     n * Z = Z
+--     n * S m = n + n * m
+--     abs = id
+--     signum Z = 0
+--     signum _ = 1
+--     fromInteger 0 = Z
+--     fromInteger n | n < 0 = error (show n ++ " is not a natural number")
+--                   | otherwise = S . fromInteger $ n - 1
+
+-- instance Integral Nat where
+--     toInteger Z = 0
+--     toInteger (S n) = 1 + toInteger n
+
+-- instance Enum Nat where
+
+-- instance Real Nat where
 
 -- Links the type- and value-level
 class Value (n :: Nat) where
@@ -27,18 +52,24 @@ instance Value Z where
 instance Value n => Value (S n) where
     value _ = S $ value (Proxy :: Proxy n)
 
-
 -- Finite totally ordered sets, indexed by the naturals
 data Fin n where
-    Zero :: Fin (S n)
+    Zero  :: Fin (S n)
     Succ :: Fin n -> Fin (S n)
+
+lift :: Fin n -> Fin (S n)
+lift Zero = Zero
+lift (Succ n) = Succ (lift n)
 
 -- Why does ghc require this to be a standalone instance declaration?
 deriving instance Show (Fin n)
 
+-- say :: forall n. Value n => Fin n -> Integer
+-- say n = toInteger $ value (Proxy :: Proxy n) - expand n
+
 expand :: Fin n -> Nat
 expand Zero = Z
-expand (Succ n) = S $ expand n
+expand (Succ x) = S $ expand x
 
 -- instance Show (Fin n) where
 --     show = show . expand
@@ -48,6 +79,31 @@ data M n = V (Fin n)
          | A (M n) (M n)
          | L (M (S n))
   deriving Show
+
+bury :: M n -> M (S n)
+bury m = case m of
+    V n -> V $ lift n
+    A f x -> A (bury f) (bury x)
+    L body -> L (bury body)
+
+substitute :: forall n. Nat -> M n -> M (S n) -> M n
+substitute depth x' body' = case body' of
+    V n -> let go :: Nat -> Fin (S m) -> Maybe (M m)
+               go Z Zero = Nothing
+               go Z (Succ m) = Just $ V m
+               go (S n) Zero = Just $ V Zero
+               go (S n) (Succ m) = go n m
+           in maybe x' id (go depth n)
+    A f x -> A (substitute depth x' f) (substitute depth x' x)
+    L body -> L (substitute (S depth) (bury x') body)
+
+-- class Sub (n :: Nat) (m :: Nat) where
+
+beta :: M n -> Maybe (M n)
+beta m = case m of
+    A (L body) x -> Just $ substitute Z x body
+    L body -> fmap L $ beta body
+    _ -> Nothing
 
 data M' = V' Id
         | A' M' M'
@@ -60,16 +116,25 @@ newtype Id = Id { unwrap :: String }
 identifier :: Parser Id
 identifier = Id <$> many1 letter_ascii
 
+identifier' :: Id -> B.Builder
+identifier' = B.string8 . unwrap
+
 m' :: Parser M'
-m' = V' <$> identifier
-  <|> parens (A' <$> m' <* space <*> m')
-  <|> parens (L' <$> (string "\\ " *> identifier <* string " . ") <*> m')
+m' = (V' <$> identifier)
+  <|> (parens (A' <$> m' <* space <*> m'))
+  <|> (parens (L' <$> (char '\\' *> identifier <* char '.') <*> m'))
   where
     parens :: Parser a -> Parser a
     parens a = char '(' *> a <* char ')'
 
-pprint :: M' -> Builder
-pprint = undefined
+pprint' :: M' -> B.Builder
+pprint' m = case m of
+    V' id -> identifier' id
+    A' x y -> parens $ pprint' x <> B.char8 ' ' <> pprint' y
+    L' id body -> parens $ B.char8 '\\' <> identifier' id <> B.char8 '.' <> pprint' body
+  where
+    parens :: B.Builder -> B.Builder
+    parens a = B.char8 '(' <> a <> B.char8 ')'
 
 dumb :: M Z -> M'
 dumb = undefined
@@ -77,5 +142,7 @@ dumb = undefined
 check :: M' -> Either String (M Z)
 check = undefined
 
-beta :: M Z -> M Z
-beta = undefined
+
+-- Testing -----------------------------------
+
+y = "(\\f.(\\x.(f (x x))) (\\x.(f (x x))))"
